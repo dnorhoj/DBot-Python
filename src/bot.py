6d6b7f9a893ad9bbebc59ad3392dcaf8dbe1a745
Daniel Nielsen <daniel@norhoj.com>
2019-02-02 14:47:52 +0100

    Initial commit v2

---
+++ src/bot.py
@@ -0,0 +1,43 @@
+import discord, config
+from discord.ext import commands
+
+async def get_prefix(bot:commands.Bot, message):
+	mention = "<@{}> ".format(bot.user.id)
+	return [mention, "+"]
+
+bot = commands.Bot(command_prefix=get_prefix)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+	print("""---Info---
+	Successfully started.
+	Running on user: {}
+	---Info---""".replace("\t", "").format(bot.user))
+	await bot.change_presence(activity=discord.Game("idk | +help"))
+
+# Define cogs to load
+startup_extensions = [
+	"cogs.help",
+	"cogs.owner",
+	"cogs.moderation",
+	"cogs.utility",
+	"cogs.fun"
+]
+
+# Load cogs
+if __name__ == "__main__":
+	lst = []
+	for extension in startup_extensions:
+		try:
+			bot.load_extension(extension)
+			lst.append(extension)
+		except Exception as e:
+			exc = "{}: {}".format(type(e).__name__, str(e))
+			print("Failed to load {}\n{}".format(extension, exc))
+	
+	out = ", ".join(lst).replace("cogs.","")[::-1].replace(",", " and"[::-1], 1)[::-1]
+	print("Loaded: {}".format(out))
+
+# Start bot
+bot.run(config.getSecret("TOKEN"))
---
+++ src/config.py
@@ -0,0 +1,84 @@
+import json, os
+
+#Change config to something else if you need to
+FOLDER = "../config/"
+CONFIG = "{}config.json".format(FOLDER)
+SERVERCONF = "{}servers.json".format(FOLDER)
+SECRET = "{}secret.json".format(FOLDER)
+
+#Gets a config value
+def getConfig(server, setting):
+	if not type(server) is str:
+		try:
+			server = server.message.server.id
+		except:
+			server = server.id
+
+	with open(SERVERCONF) as json_file:  
+		data = json.load(json_file)
+		return data[server][setting]
+
+#Sets a per-server congfig
+def setConfig(server, setting, value):
+	if not type(server) is str:
+		server = server.message.server.id
+
+	with open(SERVERCONF) as json_file:  
+		data = json.load(json_file)
+		data[server][setting] = value
+		with open(SERVERCONF, 'w') as outfile:  
+			json.dump(data, outfile, indent=4)
+
+#Gets a global config
+def getUConfig(setting):
+	with open(CONFIG) as json_file:
+		data = json.load(json_file)
+		return data[setting]
+
+#Sets a global config
+def setUConfig(setting, value):
+	with open(CONFIG) as json_file:  
+		data = json.load(json_file)
+		data[setting] = value
+		with open(CONFIG, 'w') as outfile:  
+			json.dump(data, outfile, indent=4)
+
+#Makes a template for a new server
+def setupServerConfig(server):
+	with open(SERVERCONF) as json_file:
+		data = json.load(json_file)
+		data[server.id] = {}
+		data[server.id]['name'] = server.name
+		for x in data['sample']:
+			data[server.id].update({str(x):data['sample'][x]})
+
+		with open(SERVERCONF, 'w') as outfile:  
+			json.dump(data, outfile, indent=4)
+
+def serverConfix(server):
+	with open(SERVERCONF) as json_file:
+		data = json.load(json_file)
+		try:
+			data[server.id]
+		except KeyError:
+			setupServerConfig(server)
+		return True
+
+def isDisabled(server, command):
+	with open(SERVERCONF) as json_file:
+		data = json.load(json_file)
+		if command in data[server.id]['disabled']:
+			return True
+		return False
+
+def getSecret(key):
+	try:
+		res = os.environ[key]
+	except KeyError:
+		try:
+			with open(SECRET) as json_file:
+				data = json.load(json_file)
+				res = data[key]
+		except FileNotFoundError:
+			return ""
+	return res
